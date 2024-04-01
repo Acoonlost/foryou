53a71556934625bd878aab555b47224ca29560f9
Acoonlost <156971106+Acoonlost@users.noreply.github.com>
2024-04-01 19:50:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 document.getElementById("respostaForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita que o formulário seja enviado normalmente
-      
+    event.preventDefault(); 
     var resposta = document.getElementById("resposta").value;
     
-    // Chama a função para enviar a resposta para o GitHub
     enviarRespostaParaGitHub(resposta);
     
-    // Limpa o campo de resposta
     document.getElementById("resposta").value = "";
 });
-
 function enviarRespostaParaGitHub(resposta) {
-    var token = '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'; // Substitua pelo seu token de acesso pessoal
-    var owner = 'Acoonlost'; // Substitua pelo seu nome de usuário do GitHub
-    var repo = 'foryou'; // Substitua pelo nome do seu repositório
-    var path = 'resposta.txt'; // Substitua pelo caminho do arquivo onde deseja armazenar a resposta
-    var content = btoa(resposta); // Codifica a resposta para Base64
-    
-    fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
+    var token = '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'; 
+    var owner = 'Acoonlost'; 
+    var repo = 'foryou'; 
+    var path = 'resposta.txt'; 
+    var content = btoa(resposta); 
+    fetch(https://api.github.com/repos/${owner}/${repo}/contents/${path}, {
         method: 'PUT',
         headers: {
             'Authorization': 'token ' + token,
